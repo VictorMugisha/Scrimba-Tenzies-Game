53b53483c8ca8d25ce8ece0f7d4f6e16ba487778
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-20 11:18:57 +0200

    Initialized a state to indicate completion of the game

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,17 @@ import "./App.css"
 export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            console.log("You won!")
+        }
+    }, [dice])
 
   // function allNewDice() {
   //   const newDice = []
