2be5ef51b64120f9b82cf8d8e7b032063445a997
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-20 10:18:11 +0200

    Changed the generated array to being an array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
 import React from "react"
 import Die from "./Die"
+import { nanoid } from "nanoid"
 import "./App.css"
 
 
 export default function App() {
 
-    const [dice, setDice] = React.useState(allNewDice())
-    
-    function allNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
-        }
-        return newDice
-    }
+  const [dice, setDice] = React.useState(allNewDice())
 
-    function rollDice() {
-        setDice(allNewDice())
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      })
     }
-    
-    const diceElements = dice.map(die => <Die value={die} />)
-    
-    return (
-        <main>
-            <div className="dice-container">
-                {diceElements}
-            </div>
-            <button className="roll-dice" onClick={rollDice}>Roll</button>
-        </main>
-    )
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+
+  const diceElements = dice.map(die => <Die key={die.id} value={die.value} />)
+
+  return (
+    <main>
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
+    </main>
+  )
 }
