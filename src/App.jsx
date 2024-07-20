3c2cbd8731c8d40440058add5a802628f2c54285
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-20 10:44:23 +0200

    Added half of hold die fuctionality

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,22 @@ export default function App() {
     setDice(allNewDice())
   }
 
-  const diceElements = dice.map(die => (
-    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
-  ))
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
+}
+
+const diceElements = dice.map(die => (
+    <Die 
+        key={die.id} 
+        value={die.value} 
+        isHeld={die.isHeld} 
+        holdDice={() => holdDice(die.id)}
+    />
+))
 
   return (
     <main>
