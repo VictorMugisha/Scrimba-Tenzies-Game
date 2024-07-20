31f3ac1bd2df90986952d95641c81c252a7b7995
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-20 10:53:31 +0200

    Changed the logic of rooDice function a bit

--- src/App.jsx
+++ src/App.jsx
@@ -8,51 +8,75 @@ export default function App() {
 
   const [dice, setDice] = React.useState(allNewDice())
 
+  // function allNewDice() {
+  //   const newDice = []
+  //   for (let i = 0; i < 10; i++) {
+  //     newDice.push({
+  //       value: Math.ceil(Math.random() * 6),
+  //       isHeld: false,
+  //       id: nanoid()
+  //     })
+  //   }
+  //   return newDice
+  // }
+
+  // function rollDice() {
+  //   setDice(prevDice => {
+  //     const newArray = prevDice.map(die => {
+  //       if (die.isHeld) {
+  //         return die
+  //       } else {
+  //         return {
+  //           value: Math.ceil(Math.random() * 6),
+  //           isHeld: false,
+  //           id: nanoid()
+  //         }
+  //       }
+  //     })
+  //     return newArray
+  //   })
+  // }
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
   function rollDice() {
-    setDice(prevDice => {
-        const newArray = prevDice.map(die => {
-            if (die.isHeld) {
-                return die
-            } else {
-                return {
-                    value: Math.ceil(Math.random() * 6),
-                    isHeld: false,
-                    id: nanoid()
-                }
-            }
-        })
-        return newArray
-    })
-}
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+        die :
+        generateNewDie()
+    }))
+  }
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
-        return die.id === id ? 
-            {...die, isHeld: !die.isHeld} :
-            die
+      return die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
     }))
-}
+  }
 
-const diceElements = dice.map(die => (
-    <Die 
-        key={die.id} 
-        value={die.value} 
-        isHeld={die.isHeld} 
-        holdDice={() => holdDice(die.id)}
+  const diceElements = dice.map(die => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
     />
-))
+  ))
 
   return (
     <main>
